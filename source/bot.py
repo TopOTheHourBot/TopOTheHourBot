f1242d4dd0745a327425a401c05f332546168b03
braedynl <braedynlettinga@gmail.com>
2022-11-07 12:58:11 -0500

    Cog refactoring, new segue detection system

--- source/bot.py
+++ source/bot.py
@@ -1,137 +1,69 @@
-import asyncio
-import random
-import re
+from typing import Optional
 
 from twitchio import Message
 from twitchio.ext.commands import Bot, Context, command
 
-from .utilities import TimeoutQueue, aenumerate
+from .rating_aggregator import RatingAggregator
+
+__all__ = ["TopOTheHourBot"]
 
 
 class TopOTheHourBot(Bot):
 
-    KEY_PATTERN = re.compile(r"DANKIES|PogO|TomatoTime")
-    VAL_PATTERN = re.compile(r"(-?\d+(?:\.\d*)?)\s?/\s?10")
+    # TODO: Docs
 
-    __slots__ = (
-        "moderators",
-        "timeout",
-        "count",
-        "negative_emotes",
-        "positive_emotes",
-        "mean_queue",
-        "mean_max",
-        "mean_task",
-    )
+    MODERATORS: set[str] = {"braedye", "bytesized_", "emjaye"}
 
     def __init__(
         self,
         token: str,
         *,
-        client_secret: str,
-        moderators: set[str],
-        channel: str = "hasanabi",
-        timeout: float = 9.5,
-        count: int = 20,
-        negative_emotes: tuple[str, ...] = ( "Sadge",  "FeelsBadMan",   "widepeepoSad", "unPOGGERS", "PogO", "😔"),
-        positive_emotes: tuple[str, ...] = ("Gladge", "FeelsOkayMan", "widepeepoHappy",   "POGGERS", "PogU", "😳"),
+        prefix: str = '$',
+        client_secret: Optional[str] = None,
+        initial_channels: Optional[tuple[str, ...]] = ("hasanabi",),
+        heartbeat: Optional[float] = 30,
+        retain_cache: Optional[bool] = True,
+        **kwargs,
     ) -> None:
-        super().__init__(token, client_secret=client_secret, prefix="$", initial_channels=(channel,))
-
-        self.moderators = moderators
-        self.timeout = timeout
-        self.count = count
-        self.negative_emotes = negative_emotes
-        self.positive_emotes = positive_emotes
-
-        self.mean_queue = TimeoutQueue()
-        self.mean_max   = 0.0
-        self.mean_task  = None
+        super().__init__(
+            token,
+            prefix=prefix,
+            client_secret=client_secret,
+            initial_channels=initial_channels,
+            heartbeat=heartbeat,
+            retain_cache=retain_cache,
+            **kwargs,
+        )
+        self.add_cog(RatingAggregator(self))
 
     @command()
     async def ping(self, ctx: Context) -> None:
-        await ctx.send(f"@{ctx.author.name} pong")
+        """Respond with pong"""
+        await ctx.send(f"{ctx.author.mention} pong")
 
     @command()
-    async def echo(self, ctx: Context) -> None:
-        match ctx.message.content.split():
-            case [_, *words]:
-                await ctx.send(f"@{ctx.author.name} (echo): {' '.join(words)}")
-            case _:
-                raise RuntimeError("bad invoke")
+    async def echo(self, ctx: Context, *words: str) -> None:
+        """Write a message as the bot, signifying that the message came from
+        the command's user
+        """
+        await ctx.send(f"{ctx.author.mention} (echo): {' '.join(words)}")
 
     @command()
-    async def code(self, ctx: Context) -> None:
-        match ctx.message.content.split():
-            case [_, *names]:
-                names = map(lambda name: name if name.startswith('@') else f"@{name}", names)
-                await ctx.send(f"{' '.join(names)} the bot's source code can be found on its Twitch profile Okayge")
-            case _:
-                raise RuntimeError("bad invoke")
-
-    # XXX: Debating on whether to keep this enabled or not
-
-    # async def event_ready(self):
-    #     for channel in self.connected_channels:
-    #         await channel.send(
-    #             "Bot online - use DANKIES , PogO , or TomatoTime with a score out of 10 to rate an ad segue! "
-    #             "(further conditions apply, read more on my profile)"
-    #         )
-
-    async def event_message(self, message: Message) -> None:
-        if message.echo:
-            return
-
-        name    = message.author.name
-        channel = message.channel
-        content = message.content
-
-        pending = not (self.mean_task is None or self.mean_task.done())
-
-        if (match := self.VAL_PATTERN.search(content)) and (pending or self.KEY_PATTERN.search(content)):
-            score = max(0.0, min(10.0, float(match.group(1))))
-
-            self.mean_queue.put_nowait(score)
-
-            if not pending:
-
-                async def mean():
-                    sum = 0.0
-                    async for n, score in aenumerate(
-                        self.mean_queue.consume(timeout=self.timeout),
-                        start=1,
-                    ):
-                        sum += score
-                    if n < self.count: return
-
-                    mean = round(sum / n, ndigits=2)
-                    mean_max = self.mean_max
-
-                    if mean > mean_max:
-                        self.mean_max = mean
-                        splash = "best one today, hassy!"
-                        emotes = self.positive_emotes
-                    elif mean <= 2.5:
-                        splash = "awful one, hassy"
-                        emotes = self.negative_emotes
-                    elif mean <= 5.0:
-                        splash = "good attempt, hassy"
-                        emotes = self.negative_emotes
-                    elif mean <= 7.5:
-                        splash = "nice one, hassy!"
-                        emotes = self.positive_emotes
-                    else:
-                        splash = "incredible, hassy!"
-                        emotes = self.positive_emotes
-
-                    emote = random.choice(emotes)
-
-                    await channel.send(f"DANKIES 🔔 {n} chatters rated this ad segue an average of {mean}/10 - {splash} {emote}")
-
-                self.mean_task = asyncio.create_task(mean())
-
-        if name in self.moderators:
-            await self.handle_commands(message)
+    async def code(self, ctx: Context, *names: str) -> None:
+        """Bulk-mention users to tell them where they can find the bot's source
+        code
+        """
+        names = map(lambda name: name if name.startswith('@') else f"@{name}", names)
+        await ctx.send(f"{' '.join(names)} the bot's source code can be found on its Twitch profile Okayge")
 
     async def event_command_error(self, ctx: Context, error: Exception) -> None:
-        await ctx.send(f"@{ctx.author.name} {error}")
+        """Send the error message of a command to its user"""
+        await ctx.send(f"{ctx.author.mention} {error}")
+
+    async def event_message(self, message: Message) -> None:
+        """Handle commands if the message came from a bot moderator"""
+        if message.echo:
+            return
+        moderators = self.MODERATORS
+        if message.author.name in moderators:
+            await self.handle_commands(message)
---
+++ source/rating_aggregator.py
@@ -0,0 +1,193 @@
+import asyncio
+import itertools
+import math
+import random
+import re
+from asyncio import Queue, TimeoutError
+from collections.abc import Coroutine
+from re import Pattern
+from typing import NamedTuple, Optional
+
+from twitchio import Channel, Message
+from twitchio.ext.commands import Bot, Cog
+
+__all__ = ["RatingAggregator"]
+
+
+class Payload(NamedTuple):
+    """Information pertaining to a successful aggregation
+
+    Contains the mean value, `mean`, the number of (non-unique) values used in
+    the calculation of the mean, `count`, and two booleans, `max` and `min`,
+    that indicate whether the mean was the best or worst of the current
+    runtime.
+
+    If both `max` and `min` are true, then the given `Payload` instance is
+    guaranteed to be the first succesful aggregation result. In all other
+    cases, only one of the two attributes will be true.
+    """
+    mean: float
+    count: int
+    max: bool
+    min: bool
+
+
+class RatingAggregator(Cog):
+    """A single-channel cog that averages ad segue ratings
+
+    This is primarily intended for use in HasanAbi's chat, but can be tuned for
+    others by changing the class variables below.
+    """
+
+    CHANNEL_NAME: str     = "hasanabi"  # The canonical Twitch username of the broadcaster - must be lowercased
+    CHANNEL_NICKNAME: str = "hassy"     # A nickname used in the message
+
+    TIMEOUT: float = 9.5  # The time, in seconds, for which subsequent values must be found before a message is sent
+
+    # The key and value patterns, and their respective densities.
+
+    # When aggregating, a post() task will only be dispatched if the key and
+    # value patterns have matched a number of times greater than their
+    # respective densities. When a value match occurs, aggregate() will be
+    # dispatched by event_message().
+
+    KEY: Pattern[str] = re.compile(r"DANKIES|PogO|TomatoTime")
+    VAL: Pattern[str] = re.compile(r"(-?\d+(?:\.\d*)?)\s?/\s?10")
+    KEY_DENSITY: int = 5
+    VAL_DENSITY: int = 20
+
+    # Emotes used in the post() message based on how high/low the average was.
+
+    # A positive emote is used if the average was the best of its runtime, or
+    # if the average was above 5. If neither, a negative emote is used.
+    # Emotes are selected at random with equal weighting.
+
+    NEGATIVE_EMOTES: tuple[str, ...] = (
+        "Sadge",
+        "FeelsBadMan",
+        "widepeepoSad",
+        "unPOGGERS",
+        "PogO",
+        "😔",
+        "Awkward",
+        "peepoPogO",
+        "hasCringe",
+    )
+    POSITIVE_EMOTES: tuple[str, ...] = (
+        "Gladge",
+        "FeelsOkayMan",
+        "widepeepoHappy",
+        "POGGERS",
+        "PogU",
+        "😳",
+        "pugPls",
+        "peepoPog",
+        "hasSoy",
+    )
+
+    __slots__ = ("bot", "tasks", "pool", "max", "min")
+
+    def __init__(self, bot: Bot) -> None:
+        self.bot = bot
+
+        self.tasks = {}
+        self.pool  = Queue()
+
+        self.max = -math.inf
+        self.min =  math.inf
+
+    def create_task(self, coro: Coroutine, *, name: Optional[str] = None) -> None:
+        """Create a background task from `coro`, ensuring it is garbage
+        collected
+
+        This method is primarily intended for functions of `RatingAggregator`.
+        """
+        task = asyncio.create_task(coro, name=name)
+
+        name = task.get_name()  # Get the task's name in case we weren't provided one
+        self.tasks[name] = task
+
+        task.add_done_callback(lambda task: self.tasks.pop(task.get_name()))
+
+    async def aggregate(self, channel: Channel) -> None:
+        """Wait for messages in `pool`, averaging their values until timeout,
+        and dispatch a `post()` task when appropriate
+        """
+        timeout = self.TIMEOUT
+
+        key  = self.KEY
+        val  = self.VAL
+        kmin = self.KEY_DENSITY
+        nmin = self.VAL_DENSITY
+
+        x = 0.0
+        k = 0
+
+        for n in itertools.count():
+            try:
+                message, match = await asyncio.wait_for(self.pool.get(), timeout=timeout)
+            except TimeoutError:
+                break
+            if key.search(message.content):
+                k += 1
+            x += max(0.0, min(10.0, float(match.group(1))))
+            self.pool.task_done()
+
+        if k < kmin or n < nmin:
+            return
+
+        mean = x / n
+
+        set_max = mean > self.max
+        set_min = mean < self.min
+        if set_max:
+            self.max = mean
+        if set_min:
+            self.min = mean
+
+        payload = Payload(mean, n, max=set_max, min=set_min)
+
+        self.create_task(self.post(channel, payload))
+
+    async def post(self, channel: Channel, payload: Payload) -> None:
+        """Notify `channel` of a successful aggregation result"""
+        nickname = self.CHANNEL_NICKNAME
+
+        count = payload.count
+        mean  = payload.mean
+
+        best = payload.max and not payload.min
+        if best:
+            splash = f"best one today, {nickname}!"
+        elif mean <= 2.5:
+            splash = f"awful one, {nickname}"
+        elif mean <= 5.0:
+            splash = f"good attempt, {nickname}"
+        elif mean <= 7.5:
+            splash = f"nice one, {nickname}!"
+        else:
+            splash = f"incredible, {nickname}!"
+
+        negative_emotes = self.NEGATIVE_EMOTES
+        positive_emotes = self.POSITIVE_EMOTES
+
+        emote = random.choice(positive_emotes if best or mean > 5.0 else negative_emotes)
+
+        self.create_task(channel.send(f"DANKIES 🔔 {count:d} chatters rated this ad segue an average of {mean:.2f}/10 - {splash} {emote}"))
+
+    @Cog.event()
+    async def event_message(self, message: Message) -> None:
+        """Add a message to the aggregation pool if applicable"""
+        name = self.CHANNEL_NAME
+        if message.echo or message.channel.name != name:
+            return
+
+        val = self.VAL
+        if (match := val.search(message.content)):
+            self.pool.put_nowait((message, match))
+
+            func = self.aggregate
+            if func.__name__ not in self.tasks:
+                self.create_task(
+                    func(message.channel), name=func.__name__,
+                )
