491e82b7020f4ddb2d312c27d382d0e53ac3ae28
TopOTheHourBot <topothehourbot@gmail.com>
2023-02-25 00:22:26 -0500

    Update runtime for Japan streams
    Conservatively set to 6.5 hours. Will be extended if needed.

--- main.py
+++ main.py
@@ -27,4 +27,4 @@ async def main(runtime: float = 34200) -> None:
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(main(23400))  # 6.5 hours for Japan streams (conservative estimate)
