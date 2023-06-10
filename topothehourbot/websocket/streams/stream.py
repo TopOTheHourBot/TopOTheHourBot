5e51c3dca8a576d6e308ae7822c3f9a2e1b2c964
braedynl <braedynlettinga@gmail.com>
2023-06-10 15:07:41 -0400

    Use sentinel value for stream closures, split `put()` methods

--- topothehourbot/websocket/streams/stream.py
+++ topothehourbot/websocket/streams/stream.py
@@ -3,30 +3,25 @@ from __future__ import annotations
 import asyncio
 from abc import ABCMeta, abstractmethod
 from collections import deque as Deque
-from collections.abc import AsyncIterable, AsyncIterator
-from typing import Generic, Optional, TypeVar
+from collections.abc import AsyncIterable, AsyncIterator, Awaitable
+from typing import Generic, TypeVar
 
 from .routine import routine
 
 __all__ = [
-    "CloseStream",
+    "CLOSE_STREAM",
     "IStreamBase",
     "OStreamBase",
     "IOStreamBase",
     "UnboundedIOStream",
-    "IOStream",
     "TimeboundIOStream",
 ]
+
+CLOSE_STREAM = object()  #: Sentinel value to signal stream closure
 
 T = TypeVar("T")
 T_co = TypeVar("T_co", covariant=True)
 T_contra = TypeVar("T_contra", contravariant=True)
-
-
-class CloseStream(Exception):
-    """Input stream has no further values to yield"""
-
-    __slots__ = ()
 
 
 class IStreamBase(Generic[T_co], metaclass=ABCMeta):
@@ -37,21 +32,22 @@ class IStreamBase(Generic[T_co], metaclass=ABCMeta):
     async def get(self) -> T_co:
         """Wait and return the next value of the stream
 
-        For the purpose of continuous retrieval, this method can raise
-        ``CloseStream`` to signal that further values will never be found.
+        For the purpose of continuous retrieval, this method can return
+        the ``CLOSE_STREAM`` sentinel value to signal that further significant
+        values will never be found.
         """
         raise NotImplementedError
 
     @routine
     async def get_each(self) -> AsyncIterator[T_co]:
         """Return a ``Routine`` that continuously retrieves the next stream
-        value until ``CloseStream`` is raised
+        value until ``CLOSE_STREAM`` is received
         """
-        try:
-            while True:
-                yield await self.get()
-        except CloseStream:
-            return
+        while True:
+            value = await self.get()
+            if value is CLOSE_STREAM:
+                return
+            yield value
 
 
 class OStreamBase(Generic[T_contra], metaclass=ABCMeta):
@@ -59,16 +55,24 @@ class OStreamBase(Generic[T_contra], metaclass=ABCMeta):
     __slots__ = ()
 
     @abstractmethod
-    def put(self, value: T_contra, /) -> object:
-        """Immediately place ``value`` into the stream"""
+    def put(self, value: T_contra, /) -> Awaitable[None]:
+        """Wait and place ``value`` into the stream"""
         raise NotImplementedError
 
-    async def put_each(self, values: AsyncIterable[T_contra], /) -> object:
-        """Immediately place awaited values from ``AsyncIterable`` into the
-        stream
-        """
+    @abstractmethod
+    def put_drop(self, value: T_contra, /) -> None:
+        """Place ``value`` into the stream, or drop it if at capacity"""
+        raise NotImplementedError
+
+    async def put_each(self, values: AsyncIterable[T_contra], /) -> None:
+        """Wait and place ``values`` into the stream"""
         async for value in values:
-            self.put(value)
+            await self.put(value)
+
+    async def put_drop_each(self, values: AsyncIterable[T_contra], /) -> None:
+        """Place or drop ``values`` into the stream"""
+        async for value in values:
+            self.put_drop(value)
 
 
 class IOStreamBase(IStreamBase[T_co], OStreamBase[T_contra], Generic[T_co, T_contra], metaclass=ABCMeta):
@@ -95,21 +99,11 @@ class UnboundedIOStream(IOStreamBase[T, T], Generic[T]):
             await asyncio.sleep(0)
         return self._values.popleft()
 
-    def put(self, value: T) -> object:
+    async def put(self, value: T) -> None:
         self._values.append(value)
 
-
-class IOStream(UnboundedIOStream[T, T], Generic[T]):
-
-    __slots__ = ()
-
-    def __init__(self, capacity: Optional[int] = None) -> None:
-        self._values = Deque(maxlen=capacity)
-
-    @property
-    def capacity(self) -> Optional[int]:
-        """The maximum number of values"""
-        return self._values.maxlen
+    def put_drop(self, value: T) -> None:
+        self._values.append(value)
 
 
 class TimeboundIOStream(UnboundedIOStream[T, T], Generic[T]):
@@ -129,17 +123,26 @@ class TimeboundIOStream(UnboundedIOStream[T, T], Generic[T]):
         """The duration of the stream's cooldown period"""
         return self._cooldown
 
-    def put(self, value: T) -> bool:
-        """Immediately place ``value`` into the stream if not within the
-        stream's cooldown period
-
-        Returns true if placed into the stream, otherwise false.
-        """
-        loop = asyncio.get_event_loop()
-        next_put_time = loop.time()
+    async def put(self, value: T) -> None:
+        curr_put_time = asyncio.get_event_loop().time()
         prev_put_time = self._prev_put_time
-        if (next_put_time - prev_put_time) <= self._cooldown:
-            return False
-        super().put(value)
-        self._prev_put_time = next_put_time
-        return True
+        cooldown = self._cooldown
+
+        delay = cooldown - (curr_put_time - prev_put_time)
+        await asyncio.sleep(delay)
+
+        await UnboundedIOStream.put(self, value)
+
+        self._prev_put_time = curr_put_time + delay
+
+    def put_drop(self, value: T) -> None:
+        curr_put_time = asyncio.get_event_loop().time()
+        prev_put_time = self._prev_put_time
+        cooldown = self._cooldown
+
+        if (curr_put_time - prev_put_time) <= cooldown:
+            return
+
+        UnboundedIOStream.put_drop(self, value)
+
+        self._prev_put_time = curr_put_time
