fa81d7d9360b7a59ccb31cd06c1f29964391392f
braedynl <braedynlettinga@gmail.com>
2023-10-08 01:05:52 -0400

    Change `Pipe` semantics, convert `Transport` to dataclass

--- topothehourbot/plumbing.py
+++ topothehourbot/plumbing.py
@@ -7,72 +7,44 @@ __all__ = [
 
 from abc import abstractmethod
 from collections.abc import Coroutine
-from typing import Generic, Protocol, TypeVar
+from dataclasses import dataclass
+from typing import Protocol
 
-from channels import SupportsRecv, SupportsRecvAndSend, SupportsSend
-
-T_co = TypeVar("T_co", covariant=True)
-T_contra = TypeVar("T_contra", contravariant=True)
+from ircv3 import IRCv3CommandProtocol
+from channels import SupportsRecv, SupportsSend, SupportsSendAndRecv
 
 
-class Pipe(Protocol[T_contra, T_co]):
-    """Protocol for callable, asynchronous objects that take an input and
-    output stream as arguments - a "pipe"
-    """
+class Pipe(Protocol):
 
     @abstractmethod
-    def __call__(self, istream: SupportsRecv[T_contra], ostream: SupportsSend[T_co], /) -> Coroutine:
+    def __call__(
+        self,
+        istream: SupportsRecv[IRCv3CommandProtocol],
+        omstream: SupportsSend[IRCv3CommandProtocol | str],
+        osstream: SupportsSend[IRCv3CommandProtocol | str],
+        /,
+    ) -> Coroutine:
         raise NotImplementedError
 
 
-class Transport(SupportsSend[T_contra], Generic[T_contra, T_co]):
-    """A thin wrapper around a ``Pipe`` and its input and output stream
+@dataclass(slots=True, eq=False, repr=False, match_args=False)
+class Transport:
+    """A thin wrapper around a ``Pipe``, its input stream, and output streams
 
     Note that this class does not perform any kind of task management or
     execution - it is simply used as a means to group pipes alongside their
     streams for easier object consolidation. It is up to the API user to begin
     data flow in however they see fit.
-
-    Transports are a type of output stream, and thus supports the ``send()``
-    and ``send_each()`` operations. Sends are simply forwarded to the
-    underlying ``iostream``, which is interpreted as the input stream to
-    ``pipe``.
     """
 
-    __slots__ = ("_pipe", "_iostream", "_ostream")
-    _pipe: Pipe[T_contra, T_co]
-    _iostream: SupportsRecvAndSend[T_contra, T_contra]
-    _ostream: SupportsSend[T_co]
+    pipe: Pipe
+    iostream: SupportsSendAndRecv[IRCv3CommandProtocol, IRCv3CommandProtocol]
+    omstream: SupportsSend[IRCv3CommandProtocol | str]
+    osstream: SupportsSend[IRCv3CommandProtocol | str]
 
-    def __init__(
-        self,
-        pipe: Pipe[T_contra, T_co],
-        *,
-        iostream: SupportsRecvAndSend[T_contra, T_contra],
-        ostream: SupportsSend[T_co],
-    ) -> None:
-        self._pipe = pipe
-        self._iostream = iostream
-        self._ostream = ostream
-
-    @property
-    def pipe(self) -> Pipe[T_contra, T_co]:
-        return self._pipe
-
-    @property
-    def iostream(self) -> SupportsRecvAndSend[T_contra, T_contra]:
-        return self._iostream
-
-    @property
-    def ostream(self) -> SupportsSend[T_co]:
-        return self._ostream
-
-    def send(self, value: T_contra, /) -> Coroutine:
-        """Send a value to ``iostream``"""
-        return self._iostream.send(value)
+    def send(self, command: IRCv3CommandProtocol) -> Coroutine:
+        """Send a command to ``iostream``"""
+        return self.iostream.send(command)
 
     def open(self) -> Coroutine:
-        """Call ``pipe`` with ``iostream`` as its input, and ``ostream`` as its
-        output
-        """
-        return self._pipe(self._iostream, self._ostream)
+        return self.pipe(self.iostream, self.omstream, self.osstream)
