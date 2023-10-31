d73cf1965652ec21eb219607cb485e136b1a3d82
Braedyn Lettinga <braedynlettinga@gmail.com>
2023-10-30 21:55:41 -0400

    Add `run()` method

--- topothehourbot/framework/client.py
+++ topothehourbot/framework/client.py
@@ -2,100 +2,84 @@ from __future__ import annotations
 
 __all__ = ["Client"]
 
-from collections import deque
+import contextlib
+from asyncio import TaskGroup
 from collections.abc import AsyncIterator, Coroutine, Iterable, Iterator
-from typing import Any, Final, Literal, override
+from typing import Any, Final, Literal, cast, final, override
 
-from ircv3 import (IRCv3ClientCommandProtocol, IRCv3Command,
-                   IRCv3ServerCommandProtocol)
+from ircv3 import IRCv3ClientCommandProtocol, IRCv3Command
 from ircv3.dialects.twitch import (Ping, RoomState, ServerJoin, ServerPart,
                                    ServerPrivateMessage)
-from websockets import ConnectionClosed, Data, WebSocketClientProtocol
+from websockets import ConnectionClosed, WebSocketClientProtocol
 
 from .abc import EventBroadcaster, EventListener
-from .series import series
 
 CRLF: Final[Literal["\r\n"]] = "\r\n"
 
 
-def parse_commands(data: Data) -> Iterator[IRCv3ServerCommandProtocol]:
-    if isinstance(data, bytes):
-        data = data.decode()
-    assert isinstance(data, str)
-    for command in map(
-        IRCv3Command.from_string,
-        data.rstrip(CRLF).split(CRLF)
-    ):
-        name = command.name
-        if name == "PRIVMSG":
-            yield ServerPrivateMessage.cast(command)
-        elif name == "ROOMSTATE":
-            yield RoomState.cast(command)
-        elif name == "PING":
-            yield Ping.cast(command)
-        elif name == "JOIN":
-            yield ServerJoin.cast(command)
-        elif name == "PART":
-            yield ServerPart.cast(command)
-
-
+@final
 class Client(EventBroadcaster):
 
-    __slots__ = ("_client", "_user", "_token")
+    __slots__ = ("_client", "_user", "_auth_token")
     _client: WebSocketClientProtocol
     _user: str
-    _token: str
+    _auth_token: str
 
     def __init__(
         self,
         client: WebSocketClientProtocol,
         *,
         user: str,
-        token: str,
+        auth_token: str,
         listeners: Iterable[EventListener] = (),
     ) -> None:
         super().__init__(listeners=listeners)
         self._client = client
         self._user = user
-        self._token = token
+        self._auth_token = auth_token
 
     @property
     def user(self) -> str:
         return self._user
 
-    async def send(self, command: IRCv3ClientCommandProtocol) -> None:
-        """Send a command to the Twitch IRC server"""
+    @override
+    async def event_callback(self, command: IRCv3ClientCommandProtocol) -> None:
         data = str(command)
-        try:
+        with contextlib.suppress(ConnectionClosed):
             await self._client.send(data)
-        except ConnectionClosed:
-            return  # Let recv() propagate
 
-    async def recv(self, *, memo: deque[IRCv3ServerCommandProtocol] = deque()) -> IRCv3ServerCommandProtocol:
-        """Receive a command from the Twitch IRC server
+    def on_ping(self, ping: Ping) -> Coroutine[Any, Any, None]:
+        return self.event_callback(ping.reply())
 
-        Raises ``RuntimeError`` if called from two coroutines concurrently, or
-        ``websockets.ConnectionClosed`` if the underlying connection was
-        closed.
-        """
-        if memo:
-            return memo.popleft()
-        commands = parse_commands(await self._client.recv())
-        command = next(commands)
-        memo.extend(commands)
-        return command
+    def _parse_commands(self, data: str) -> Iterator[Coroutine[Any, Any, None]]:
+        raw_commands = data.rstrip(CRLF).split(CRLF)
+        for command in map(IRCv3Command.from_string, raw_commands):
+            name = command.name
+            if name == "PRIVMSG":
+                yield self.on_message(ServerPrivateMessage.cast(command))
+            elif name == "ROOMSTATE":
+                yield self.on_room_state(RoomState.cast(command))
+            elif name == "PING":
+                yield self.on_ping(Ping.cast(command))
+            elif name == "JOIN":
+                yield self.on_join(ServerJoin.cast(command))
+            elif name == "PART":
+                yield self.on_part(ServerPart.cast(command))
 
-    @series
-    async def recv_all(self) -> AsyncIterator[IRCv3ServerCommandProtocol]:
-        """Return an asynchronous iterator that continuously receives commands
-        from the Twitch IRC server until the connection has closed
-        """
-        try:
+    async def _parse_data(self) -> AsyncIterator[Coroutine[Any, Any, None]]:
+        with contextlib.suppress(ConnectionClosed):
             while True:
-                yield await self.recv()
+                data = cast(str, await self._client.recv())
+                for coro in self._parse_commands(data):
+                    yield coro
+
+    async def run(self) -> None:
+        try:
+            await self._client.send("CAP REQ :twitch.tv/commands twitch.tv/membership twitch.tv/tags")
+            await self._client.send(f"PASS oauth:{self._auth_token}")
+            await self._client.send(f"NICK {self._user}")
         except ConnectionClosed:
             return
-
-    @override
-    def event_callback(self, command: IRCv3ClientCommandProtocol) -> Coroutine[Any, Any, None]:
-        return self.send(command)
+        async with TaskGroup() as tasks:
+            async for coro in self._parse_data():
+                tasks.create_task(coro)
