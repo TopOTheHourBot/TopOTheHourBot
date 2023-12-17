21aa909c4be3b8d6bfe7e6ee3687d859840f395c
Braedyn Lettinga <braedynlettinga@gmail.com>
2023-12-17 13:47:20 -0500

    Fix unhandled scenario for multiple sends

--- topothehourbot/system/pipes/pipe.py
+++ topothehourbot/system/pipes/pipe.py
@@ -6,7 +6,7 @@ __all__ = [
 ]
 
 import asyncio
-from asyncio import Future, InvalidStateError
+from asyncio import Future
 from collections import deque as Deque
 from collections.abc import AsyncIterator
 from typing import Optional, final
@@ -52,8 +52,12 @@ class Pipe[T]:
         if self.is_closed():
             raise Closure
         self._buffer.append(value)
-        if (receiver := self._receiver):
-            receiver.set_result(None)
+        receiver = self._receiver
+        if receiver is None:
+            return
+        if receiver.done():
+            return
+        receiver.set_result(None)
 
     async def recv(self) -> T:
         """Receive a value from the pipe
@@ -78,16 +82,18 @@ class Pipe[T]:
     def close(self) -> None:
         """Close the pipe
 
-        Raises ``Closure`` into the active ``recv()`` call (if one exists) and
-        clears the pipe buffer.
+        Raises ``Closure`` into the active ``recv()`` call (if one exists).
         """
-        try:
-            self._closer.set_result(None)
-        except InvalidStateError:
+        closer = self._closer
+        if closer.done():
             return
-        if (receiver := self._receiver):
-            receiver.set_exception(Closure)
-        self.clear()
+        closer.set_result(None)
+        receiver = self._receiver
+        if receiver is None:
+            return
+        if receiver.done():
+            return
+        receiver.set_exception(Closure)
 
     def clear(self) -> None:
         """Clear the pipe
