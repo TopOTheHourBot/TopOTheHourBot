b28d22196d03ffd8addbbd47265faf929d83a3b8
Braedyn Lettinga <braedynlettinga@gmail.com>
2023-12-18 23:49:57 -0500

    Execution instructions

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+"""Execute TopOTheHourBot
+
+Typical execution and closure from command line:
+
+```bash
+$ python3.12 -u -OO ./main.py > ./main.log 2>&1 &
+$ pkill -SIGINT [PID]
+```
+
+Flag ``-u`` is required to force stdout and stderr to be unbuffered. Nothing
+will be written to the log if not provided. Change the logging level in the
+``basicConfig()`` call below (note that ``DEBUG`` logs all input and output,
+which is probably never wanted for deployment).
+
+Flag ``-OO`` is optional and removes all ``assert`` and ``__debug__``-dependent
+statements (which are used pretty frequently).
+"""
+
 import asyncio
 import logging
 import sys
