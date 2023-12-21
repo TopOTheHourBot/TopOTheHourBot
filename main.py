f1139309a8b7abe488f053e3ab7f4967f67b55bb
Braedyn Lettinga <braedynlettinga@gmail.com>
2023-12-20 19:53:47 -0500

    Add CLI

--- main.py
+++ main.py
@@ -1,30 +1,29 @@
-"""Execute TopOTheHourBot
+"""The TopOTheHourBot CLI
 
-Typical execution and closure from command line:
+Background execution and closure example, with log file:
 
 ```bash
-$ python3.12 -u -OO ./main.py > ./main.log 2>&1 &
+$ python3.12 -u -OO ./main.py "$TWITCH_OAUTH_TOKEN" > ./main.log 2>&1 &
 $ kill -SIGINT [PID]
 ```
 
 Flag ``-u`` is required to force stdout and stderr to be unbuffered. Nothing
 will be written to the log if not provided. Change the logging level in the
-``basicConfig()`` call below (note that ``DEBUG`` logs all input and output,
-which is probably never wanted for deployment).
+``basicConfig()`` call below if desired (note that ``DEBUG`` logs all input and
+output, which should never be used for deployment).
 
-Flag ``-OO`` is optional and removes all ``assert`` and ``__debug__``-dependent
-statements (which are used pretty frequently).
-
-I do *not* advise killing the bot's process through means other than
-``kill -SIGINT``, as this command will raise a ``KeyboardInterrupt`` into the
-program that allows for proper clean-up and state saves to occur.
+Flag ``-OO`` is recommended to remove all ``assert`` and ``__debug__``-
+dependent statements, which can speed up processing (ircv3 makes extensive use
+of ``assert`` statements during ``cast()``s).
 """
 
 import asyncio
 import logging
 import sys
+from argparse import ArgumentParser
+from pathlib import Path
 
-from topothehourbot import main
+import topothehourbot
 
 logging.basicConfig(
     format="[%(asctime)s] [%(name)s] %(message)s",
@@ -33,5 +32,40 @@ logging.basicConfig(
 )
 
 
+def main() -> None:
+    parser = ArgumentParser(
+        prog="main",
+        description="Execute TopOTheHourBot",
+        epilog="Use `kill -SIGINT [PID]` to close TopOTheHourBot from the"
+               " background. This simulates a KeyboardInterrupt, allowing"
+               " exception handlers (like those used to save pickles) to run.",
+    )
+
+    parser.add_argument(
+        "oauth-token",
+        help="A Twitch IRC OAuth token. Storing the token in the system"
+             " environment variables and invoking it by its name is advised."
+             " See here for details on generating one:"
+             " https://dev.twitch.tv/docs/irc/authenticate-bot/",
+    )
+    parser.add_argument(
+        "--pickle-directory",
+        type=Path,
+        default=topothehourbot.DEFAULT_PICKLE_DIRECTORY,
+        help="A local directory to read and write pickled data between sessions"
+             " of execution. Defaults to topothehourbot/pickles/. The directory"
+             " is created if it does not already exist.",
+    )
+
+    namespace = parser.parse_args()
+
+    main_coro = topothehourbot.main(
+        namespace.oauth_token,
+        pickle_directory=namespace.pickle_directory,
+    )
+
+    return asyncio.run(main_coro)
+
+
 if __name__ == "__main__":
-    asyncio.run(main("..."))
+    main()
