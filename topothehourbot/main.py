30f90e404b829d0152d2e57296194c4a98f71dcf
Braedyn Lettinga <braedynlettinga@gmail.com>
2023-12-24 20:22:49 -0500

    Rename save and load functions

--- topothehourbot/main.py
+++ topothehourbot/main.py
@@ -24,8 +24,8 @@ class HasanAbiExtensionParameters:
     roleplay_rating_total: int = 0
 
 
-def load_object[DefaultT](path: Path, *, default: DefaultT = None) -> Any | DefaultT:
-    """Load a Python object from ``path``
+def load_pickle[DefaultT](path: Path, *, default: DefaultT = None) -> Any | DefaultT:
+    """Load a pickled object from ``path``
 
     Returns ``default`` if the file pointed to by ``path`` is not found.
     """
@@ -36,8 +36,12 @@ def load_object[DefaultT](path: Path, *, default: DefaultT = None) -> Any | Defa
         return default
 
 
-def save_object(path: Path, object: Any, *, protocol: Optional[int] = pickle.HIGHEST_PROTOCOL) -> None:
-    """Save a Python object to ``path``"""
+def save_pickle(path: Path, object: Any, *, protocol: Optional[int] = pickle.HIGHEST_PROTOCOL) -> None:
+    """Save a pickled object to ``path``
+
+    ``protocol`` argument passed to ``pickle.dump()`` - see its docstring for
+    more details.
+    """
     with open(path, mode="wb") as file:
         pickle.dump(object, file, protocol)
 
@@ -62,7 +66,7 @@ async def main(
     assert pickle_directory.is_dir()
     pickle_directory.mkdir(exist_ok=True)
     async for client in system.connect(TopOTheHourBot, oauth_token=oauth_token):
-        parameters: HasanAbiExtensionParameters = load_object(
+        parameters: HasanAbiExtensionParameters = load_pickle(
             pickle_directory / "HasanAbi.pickle",
             default=HasanAbiExtensionParameters(),
         )
@@ -75,7 +79,7 @@ async def main(
                 tasks.create_task(extension.distribute())
                 await client.distribute()
         finally:
-            save_object(
+            save_pickle(
                 pickle_directory / "HasanAbi.pickle",
                 HasanAbiExtensionParameters(extension.roleplay_rating_total),
             )
